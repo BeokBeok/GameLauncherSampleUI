b8240d1201d5c9f25a95bcfd64bff7792e90c5ac
강현석 <kekemusa37@gmail.com>
2021-08-11 23:01:35 +0900

    :lipstick: 최근 게임 헤더

--- app/src/main/java/com/beok/gamelaunchersampleui/MainActivity.kt
+++ app/src/main/java/com/beok/gamelaunchersampleui/MainActivity.kt
@@ -3,8 +3,10 @@ package com.beok.gamelaunchersampleui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -14,16 +16,28 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material.icons.filled.ThumbUp
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
@@ -49,14 +63,51 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     fun MainContent() {
-        TopAppBar(
-            title = { Text(text = "게임 런처") },
-            backgroundColor = Color.Transparent,
-            navigationIcon = {
-                IconButton(onClick = { }) {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = { Text(text = "게임 런처") },
+                    backgroundColor = Color.Transparent,
+                    navigationIcon = {
+                        IconButton(onClick = { }) {
+                            Icon(
+                                imageVector = Icons.Default.Menu,
+                                contentDescription = null
+                            )
+                        }
+                    }
+                )
+            },
+            content = {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                ) {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .weight(5F)
+                    ) {
+                        Text(text = "최근 게임")
+                        Icon(
+                            imageVector = Icons.Default.KeyboardArrowRight,
+                            contentDescription = null
+                        )
+                    }
                     Icon(
-                        imageVector = Icons.Filled.Menu,
-                        contentDescription = null
+                        imageVector =  Icons.Default.Person,
+                        contentDescription = null,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .weight(1F)
+                    )
+                    Icon(
+                        imageVector =  Icons.Default.Notifications,
+                        contentDescription = null,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .weight(1F)
                     )
                 }
             }
