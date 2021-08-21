93db407a4104631fdf3cad8570bef0fb29075dc2
강현석 <kekemusa37@gmail.com>
2021-08-21 18:02:46 +0900

    :lipstick: 최근 게임 컨텐츠

--- app/src/main/java/com/beok/gamelaunchersampleui/MainActivity.kt
+++ app/src/main/java/com/beok/gamelaunchersampleui/MainActivity.kt
@@ -4,58 +4,47 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.ScrollableState
-import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Card
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.material.TextField
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
-import androidx.compose.material.icons.filled.ArrowForward
-import androidx.compose.material.icons.filled.CheckCircle
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.Phone
-import androidx.compose.material.icons.filled.ThumbUp
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
-import androidx.compose.ui.input.nestedscroll.NestedScrollSource
-import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 import com.beok.gamelaunchersampleui.ui.theme.GameLauncherSampleUITheme
-import kotlin.math.roundToInt
 
 class MainActivity : ComponentActivity() {
 
@@ -90,9 +79,57 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.verticalScroll(rememberScrollState())
                 ) {
                     Header()
+                    RecentGameContent()
                 }
             }
         )
+    }
+
+    @Composable
+    private fun RecentGameContent() {
+        LazyRow(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+                .padding(vertical = 12.dp)
+                .padding(start = 16.dp),
+            contentPadding = PaddingValues(end = 0.dp)
+        ) {
+            repeat(5) {
+                item {
+                    Card(
+                        modifier = Modifier
+                            .width(140.dp)
+                            .padding(end = 16.dp)
+                            .fillMaxHeight(),
+                        shape = RoundedCornerShape(20.dp),
+                        elevation = 0.dp,
+                        backgroundColor = Color.LightGray
+                    ) {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(all = 12.dp)
+                        ) {
+                            Image(
+                                painter = rememberImagePainter(
+                                    data = "https://play-lh.googleusercontent.com/DpN3DRIsAyYQB_URf7PEmvgLdse51tM_ddRyL7a0MS2gjfl30cBheFmx8Uc8W9qU6sWT=s180-rw"
+                                ),
+                                contentDescription = null,
+                                modifier = Modifier
+                                    .padding(bottom = 4.dp)
+                                    .width(72.dp)
+                                    .height(72.dp)
+                                    .clip(shape = RoundedCornerShape(20.dp))
+                            )
+                            Text(text = "냥코 대전쟁", color = Color.White)
+                            Text(text = "33:19", color = Color.White)
+                            Text(text = "1시간 전", color = Color.White)
+                        }
+                    }
+                }
+            }
+        }
     }
 
     @Composable
